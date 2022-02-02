e29692e167fd7c890c8177a9ef95bce334068ce3
SrikarGathey1 <venkatnaras123@gmail.com>
2022-02-02 19:45:35 +0530

    Delete Implementation directory

--- Implementation/compile.sh
+++
@@ -1,5 +0,0 @@
-gcc -g -c shm_demo.c -o shm_demo.o
-gcc -g -c otto.c -o otto.o
-gcc -g -c oskar.c -o oskar.o
-gcc -g oskar.o shm_demo.o -o oskar -lrt
-gcc -g otto.o shm_demo.o -o otto -lrt                                    
